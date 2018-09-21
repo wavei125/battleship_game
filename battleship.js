6cdde059854c2e2ee215ea5ef452a7d50a68b948
Nguyen Vinh <dylan00433@gmail.com>
2018-09-22 00:50:37 +0700

    fix js

--- battleship.js
+++ battleship.js
@@ -3,22 +3,21 @@ var score = 0;
 var round = 0;
 var xUser;
 var yUser;
-if(score < 3){
-    for (let n = 0; n <= 4; n++) {
-        xUser = prompt("enter number 0 to 10:");
-        yUser = prompt("enter number 0 to 10:");
-        round = round + 1;
-        for(let i = 0; i < ships.length; i++){
-            if(xUser = ships[i].x && yUser == ships[i].y){
-                score = score + 1;
-                ships.splice(i, 1);
-                alert("Hit shot!");
-            }
-        }
-    }
+for (let n = 1; n <= 5; n++) {
+	xUser = prompt("x - enter number 0 to 10:");
+	yUser = prompt("y - enter number 0 to 10:");
+	round = round + 1;
+  alert("round is " + round);
+	for(let i = 0; i < ships.length; i++){
+    if(xUser = ships[i].x && yUser == ships[i].y){
+			score = score + 1;
+      ships.splice(i, 1);
+      alert("Hit shot!");
+     }
+  }
 }
-if(score == 3){
-    alert("You win");
+if(score >= 3){
+  alert("You win");
 } else {
-    alert("You lose");
+  alert("You lose");
 }
